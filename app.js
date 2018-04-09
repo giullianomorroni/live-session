1111eb54ba67592b17dcccb8defcb037d409c4a9
Giulliano Morroni <giulliano.morroni@yara.com>
2018-04-09 15:04:17 -0300

    updates node

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var app     = express();
+var path    = require("path");
+
+
+app.get('/',function(req,res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.get('/about',function(req,res){
+  res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/review',function(req,res){
+  res.sendFile(__dirname + '/review.html');
+});
+
+app.get('/joinus',function(req,res){
+  res.sendFile(__dirname + '/joinus.html');
+});
+
+app.get('/contact',function(req,res){
+  res.sendFile(__dirname + '/contact.html');
+});
+
+app.use(express.static('assets'));
+
+app.listen(3000);
+
+console.log("Running at Port 3000");
